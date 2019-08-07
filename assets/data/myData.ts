f6d382ad27fd6015cc84e685d4f66f66801a32a9
stephenAJ <50453083+stephenAJ@users.noreply.github.com>
2019-08-07 14:37:45 -0400

    Add files via upload

---
+++ assets/data/myData.ts
@@ -0,0 +1,9 @@
+import {Jimenest} from '../../app/jimenest';
+
+export const JIMENEST: Jimenest = {
+    studentNum: "991290203",
+    studentName: "Stephen Jimenez",
+    username: "jimenest",
+    campus: "Trafalgar",
+    assignTitle: "Assignment 4"
+}
